6cd34d6524dbbf2b5938bd69bfb00f4ed6a6fdb2
kinokano <kin2014@hotmail.com>
2025-03-13 17:07:56 -0400

    A

--- core/settings.py
+++ core/settings.py
@@ -75,10 +75,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Django',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT': '3306',
     }
 }
 
